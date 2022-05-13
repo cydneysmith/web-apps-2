26d949a55d94a3cdfd0c42248cee3bdeff02a4f9
cydneysmith <cydney.smith@kellogg.northwestern.edu>
2022-05-13 21:31:59 +0000

    done with class

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,4 +9,11 @@ class ContactsController < ApplicationController
     @contact = Contact.new
   end
 
+  def create
+    @contact = Contact.new
+    @contact["first_name"] = params["contact"]["first_name"]
+    @contact["last_name"] = params["contact"]["last_name"]
+    @contact["email"] = params["contact"]["email"]
+    @contact.saveredirect_to "/companies"
+  end
 end
